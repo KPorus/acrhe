f895073e319ee17d3bef44a6e5eada8cda587f39
KPorus <designerfardin2021@gmail.com>
2022-12-21 20:05:58 +0600

    service part half complete

--- src/component/ServicesSection.jsx
+++ src/component/ServicesSection.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import CommonHead from "./CommonHead";
-import img from "../images/services.jpg";
 import "../App.css";
+import img from "../images/services.jpg";
+import img1 from "../images/service-1.jpg";
+import img2 from "../images/service-2.jpg";
+import { Link } from "react-router-dom";
+
+const serviceBanner = [
+  {
+    img: img1,
+    head: "Architecture",
+    body: "Whole building structure we made or shape it as livelihood",
+  },
+  {
+    img: img2,
+    head: "Interior Design",
+    body: "Only  room’s inside structure we made or shape it as livelihood",
+  },
+];
 const ServicesSection = () => {
   return (
-    <div className=''>
+    <div className='my-20'>
       <div
-        className='servicebanner font-montserrat mt-20 py-6'
+        className='servicebanner font-montserrat py-20'
         style={{
           background: `url(${img})`,
           backgroundPosition: "center",
@@ -16,18 +32,19 @@ const ServicesSection = () => {
         <div>
           <CommonHead></CommonHead>
         </div>
-        <div className=' z-1 flex justify-center gap-4 my-3 container mx-auto'>
-          <div className='w-96 shadow-xl'>
-            <figure>
-              <img src='https://placeimg.com/400/225/arch' alt='Shoes' />
-            </figure>
-          </div>
-
-          <div className='w-96 shadow-xl'>
-            <figure>
-              <img src='https://placeimg.com/400/225/arch' alt='Shoes' />
-            </figure>
-          </div>
+        <div className='flex justify-evenly gap-4 my-3 container mx-auto'>
+          {serviceBanner.map((items) => (
+            <div className='flex shadow-xl'>
+              <figure className="relative ">
+                <img src={items.img} alt='service' className='w-full' />
+              </figure>
+              <div className="absolute w-80 bg-slate-50 bottom-10 p-4 font-montserrat">
+                <h1 className="text-[#d9b257] font-semibold">{items.head}</h1>
+                <p className="text-[#9999] font-bold">{items.body}</p>
+                <Link to="/project" className="text-center border-b-4 border-[#d9b257]">Know more</Link>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>

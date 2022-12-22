d633d47369db9af8ae7f60a52c80a594e1bf02b3
KPorus <designerfardin2021@gmail.com>
2022-12-22 22:23:13 +0600

    about page

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -1,11 +1,24 @@
-import React from 'react';
-
+import React from "react";
+import img from "../../images/Aboutbanner.jpg";
+import "../../App.css";
+import { FaAngleRight } from "react-icons/fa";
 const About = () => {
-    return (
-        <div id='about'>
-            hisdfdsfdfd
-        </div>
-    );
+  return (
+    <div
+      className='aboutbanner relative py-6'
+      style={{
+        background: `url(${img})`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        zIndex: "1",
+      }}>
+      <div className='text-center w-1/2 mx-auto '>
+        <h1 className='text-[1.5rem] font-bold text-[#d9b257]'>Our Services</h1>
+       <h4 className="flex items-center justify-center text-white">Home  <FaAngleRight className="text-white"/> <span className="text-[#d9b257]">About us</span></h4>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Footer from '../component/Footer';
-import Head from '../component/Head';
-import Nav from '../component/Nav';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../component/Footer";
+import Head from "../component/Head";
+import Nav from "../component/Nav";
 
 const Main = () => {
-    return (
-        <div>
-            <Head></Head>
-            <Nav></Nav>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <Head></Head>
+      <Nav></Nav>
+      <div className='min-h-screen'>
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
